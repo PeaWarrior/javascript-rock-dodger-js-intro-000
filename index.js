ada1ee423b4008fa1751bc500c039164fdad76c1
Jackson Chen <jacksonchen171@gmail.com>
2020-03-29 02:37:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -111,7 +111,9 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval)
-  // ROCKS = []
+  ROCKS.forEach(function(rock){
+    rock.remove()
+  })
 }
 
 function moveDodger(e) {
