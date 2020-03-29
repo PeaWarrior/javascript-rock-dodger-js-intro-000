d9052ccc33162f59531c0c81dbefbccfec876bed
Jackson Chen <jacksonchen171@gmail.com>
2020-03-29 01:39:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function createRock(x) {
      */
      else {
        if (top < 200) {
-         window.requestAnimationFrame(moveRock(), 1000)
+         window.requestAnimationFrame(moveRock())
        }
      }
     /**
