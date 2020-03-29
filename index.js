729fc5be668ae86b0d9f6c09fa9b646a64ea9dae
Jackson Chen <jacksonchen171@gmail.com>
2020-03-29 03:32:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -156,7 +156,6 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   var leftNumber = positionToInteger(DODGER.style.left)
-  console.log(`${leftNumber}px`)
   if (leftNumber <= 356) {
     DODGER.style.left = `${leftNumber += 4}px`
     window.requestAnimationFrame(moveDodgerRight)
