338e3677869a8f51de93beeaf6a9828b2c259312
Jackson Chen <jacksonchen171@gmail.com>
2020-03-29 01:38:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,9 @@ function createRock(x) {
    * seems like a good pace.)
    */
   function moveRock() {
+    if (top < 200) {
     rock.style.top = `${top += 2}px`
+    }
     // implement me!
     // (use the comments below to guide you!)
     /**
